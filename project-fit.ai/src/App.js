7ab267ebc2e73c140b033bdac040a0f7dfb81485
Brian Liao <brianliao@Brians-MacBook-Air-7.local>
2023-01-17 19:09:21 -0800

    added api config

--- project-fit.ai/src/App.js
+++ project-fit.ai/src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
-import './App.scss';
-import { ChakraProvider } from '@chakra-ui/react'
-import Navbar from './components/Navbar';
-import Form from './components/Form';
+import './App.css';
+
 function App() {
   return (
-    <ChakraProvider>
-      <div className="App">
-
-        <Navbar/>
-        <Form />
-      </div>
-    </ChakraProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
