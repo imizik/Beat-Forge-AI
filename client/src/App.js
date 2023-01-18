2efac7d7d23e495db1b9109c5be0a7c1bff90296
imizik <chung.isaac3@gmail.com>
2023-01-17 19:16:13 -0800

    readded

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.scss';
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from './components/Navbar';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <div className="App">
+
+        <Navbar/>
+        <Form />
+      </div>
+  </ChakraProvider>
+  );
+}
+
+export default App;
