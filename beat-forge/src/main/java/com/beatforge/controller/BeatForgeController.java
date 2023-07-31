462a0c91956f3fbd4ee680874084cd37ed7478f7
imizik <chung.isaac3@gmail.com>
2023-07-30 22:29:21 -0700

    ls

--- beat-forge/src/main/java/com/beatforge/controller/BeatForgeController.java
+++ beat-forge/src/main/java/com/beatforge/controller/BeatForgeController.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +16,6 @@ import com.beatforge.service.OpenAIService;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*")
 public class BeatForgeController {
     private final OpenAIService openAIService;
 
