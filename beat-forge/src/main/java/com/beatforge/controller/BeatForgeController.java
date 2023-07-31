bf378eb803cd44afcff49e37ffef34122a7dc758
imizik <chung.isaac3@gmail.com>
2023-07-30 20:59:36 -0700

    cdtest4

--- beat-forge/src/main/java/com/beatforge/controller/BeatForgeController.java
+++ beat-forge/src/main/java/com/beatforge/controller/BeatForgeController.java
@@ -43,7 +43,7 @@ public class BeatForgeController {
 
     @GetMapping("/")
     public String helloWorld() {
-        logger.info("Receive req fo hello world");
+        logger.info("Receive req forr hello world");
         return "Hello World!";
     }
 }
