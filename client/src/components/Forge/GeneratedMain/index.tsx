b2832564167860e4eac3940e73b3cf0bff9c8c90
imizik <chung.isaac3@gmail.com>
2023-08-21 21:54:11 -0700

    redidbox

--- client/src/components/Forge/GeneratedMain/index.tsx
+++ client/src/components/Forge/GeneratedMain/index.tsx
@@ -51,109 +51,61 @@ export const GeneratedMain: GeneratedMainComponent = ({ data, onGoBack }) => {
       color="text.primary"
       width="100%"
       alignItems="center"
+      justifyContent="space-between"
+      minHeight="100vh"
     >
       <Navbar bgProp="secondary.main" />
-      <Box
-        as="header"
-        justifyContent="space-between"
-        alignItems="center"
-        p={5}
-      >
-        <Image src="/path/to/logo.png" alt="Logo" h="50px" />
-        <Box as="nav">
-          <Link href="#" mr={5}>
-            Home
-          </Link>
-          <Link href="#">About</Link>
-        </Box>
-      </Box>
-      <Box as="main" p={5}>
-        <EditableArea data={title} setData={setTitle} />
-        <SimpleGrid columns={2} spacing={10} my={5} color="black">
-          <Box
-            bg="white"
-            p={5}
-            shadow="md"
-            borderWidth="1px"
-            flex="1"
-            borderRadius="md"
-          >
+      <Stack className="stack-ctn">
+      <div>Generated Beat Information:</div>
+
+        <Box
+          p={5}
+          shadow="md"
+          borderWidth="1px"
+          borderRadius="md"
+          mx={10}
+        >
+          <EditableArea data={title} setData={setTitle} />
+          <Flex flexDirection="column" alignItems="flex-start" spacing={3}>
             <Text>
               <FaMeteor /> BPM: {data.bpm}
             </Text>
-          </Box>
-          <Box
-            bg="white"
-            p={5}
-            shadow="md"
-            borderWidth="1px"
-            flex="1"
-            borderRadius="md"
-          >
             <Text>
               <FaMusic /> Key: {data.key}
             </Text>
-          </Box>
-          <Box
-            bg="white"
-            p={5}
-            shadow="md"
-            borderWidth="1px"
-            flex="1"
-            borderRadius="md"
-          >
             <Text>
-              <FaChartBar /> Chord Progression:{' '}
-              {data.chordProgression.join(', ')}
+              <FaChartBar /> Chords: {data.chordProgression.join(', ')}
             </Text>
-          </Box>
-          <Box
-            bg="white"
-            p={5}
-            shadow="md"
-            borderWidth="1px"
-            flex="1"
-            borderRadius="md"
-          >
             <Text>Bars: {data.bars}</Text>
-          </Box>
-          <Box
-            bg="white"
-            p={5}
-            shadow="md"
-            borderWidth="1px"
-            flex="1"
-            borderRadius="md"
-          >
             <Text>Timing: {data.timing.join(', ')}</Text>
-          </Box>
-        </SimpleGrid>
-        <Flex justify="space-between">
-          <IconButton
-            aria-label="Go back"
-            icon={<ArrowBackIcon />}
-            colorScheme="whiteAlpha"
-            variant="ghost"
-            onClick={onGoBack}
-            _hover={{ color: 'teal.500' }}
-          />
-          <Button
-            colorScheme="purple"
-            onClick={handleMidiGenerateClick}
-            _hover={{ bg: 'purple.500' }}
-          >
-            Generate MIDI
-          </Button>
-          <Button
-            leftIcon={<FaPlay />}
-            colorScheme="purple"
-            onClick={handleGenerateClick}
-            _hover={{ bg: 'purple.500' }}
-          >
-            Play Sound
-          </Button>
-        </Flex>
-      </Box>
+          </Flex>
+          <Flex justify="space-between" mt={5}>
+            <IconButton
+              aria-label="Go back"
+              icon={<ArrowBackIcon />}
+              colorScheme="whiteAlpha"
+              variant="ghost"
+              onClick={onGoBack}
+              _hover={{ color: 'teal.500' }}
+            />
+            <Button
+              colorScheme="purple"
+              onClick={handleMidiGenerateClick}
+              _hover={{ bg: 'purple.500' }}
+            >
+              Generate MIDI
+            </Button>
+            <Button
+              leftIcon={<FaPlay />}
+              colorScheme="purple"
+              onClick={handleGenerateClick}
+              _hover={{ bg: 'purple.500' }}
+            >
+              Play Sound
+            </Button>
+          </Flex>
+        </Box>
+      </Stack>
       <Footer />
     </Stack>
   );
